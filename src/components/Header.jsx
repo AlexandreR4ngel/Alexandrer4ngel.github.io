0092aa14c7fea3d58b440429587abc0c82f795a4
Alexandre Magno Rangel <99764226+AlexandreR4ngel@users.noreply.github.com>
2023-06-21 18:38:43 +0000

    react icons no header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,12 @@
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/fa'; 
+import { GrLinkedin, GrGithub } from "react-icons/gr"; //https://react-icons.github.io/react-icons/icons?name=gr
 
 export default function Header() {
     return (
         <Header>
             <h2>Alexandre Magno Rangel</h2>
             <h3>Desenvolver web fullstack</h3>
-            <AiFillGithub />
-            <AiFillInstagram />
-            <AiFillLinkedin />
+            <GrLinkedin />
+            <GrGithub />
         </Header>
     )
 }
