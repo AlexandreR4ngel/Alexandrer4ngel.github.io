b5d9575775a5ea9e84f80658699a8aa94ed8ed46
Alexandre Magno Rangel <99764226+AlexandreR4ngel@users.noreply.github.com>
2023-06-22 19:13:40 +0000

    organizando divs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,15 +4,17 @@ import { BsLinkedin, BsGithub } from "react-icons/bs"; // https://react-icons.gi
 export default function Header() {
     return (
         <div className='SectionHeader'>
-            <p>Sobre</p>
-            <p>Qualificação</p>
-            <p>Projetos</p>
-            <a href="https://www.linkedin.com/in/alexandre-magno-rangel-193842222/" target="blank">
-                <BsLinkedin />
-            </a>
-            <a href="https://github.com/AlexandreR4ngel" target="blank">
-                <BsGithub />
-            </a>
+            <div className='divHeader'>
+                <p>Sobre</p>
+                <p>Qualificação</p>
+                <p>Projetos</p>
+                <a href="https://www.linkedin.com/in/alexandre-magno-rangel-193842222/" target="blank">
+                    <BsLinkedin />
+                </a>
+                <a href="https://github.com/AlexandreR4ngel" target="blank">
+                    <BsGithub />
+                </a>
+            </div>
         </div>
     )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,17 +4,34 @@ export default function Main() {
     return (
         <div className="SectionMain">
             <div>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vehicula tristique luctus. 
-                Nunc porttitor justo mollis, imperdiet nunc vel, varius elit.Vivamus aliquet ex ut ex efficitur
-                finibus ut vel ipsum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla vel
-                laoreet arcu. Cras vitae dui nisl. Vestibulum quam dolor, malesuada id consectetur ac, fringilla
-                lobortis odio. Morbi nec leo varius, iaculis dui eu, porttitor eros. Fusce blandit pellentesque consequat.
+                <h3>Sobre</h3>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vehicula tristique luctus. 
+                    Nunc porttitor justo mollis, imperdiet nunc vel, varius elit.Vivamus aliquet ex ut ex efficitur
+                    finibus ut vel ipsum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla vel
+                    laoreet arcu. Cras vitae dui nisl. Vestibulum quam dolor, malesuada id consectetur ac, fringilla
+                    lobortis odio. Morbi nec leo varius, iaculis dui eu, porttitor eros. Fusce blandit pellentesque consequat.
+                </p>
             </div>
             <div>
                 <h3>Capacitação</h3>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vehicula tristique luctus. 
+                    Nunc porttitor justo mollis, imperdiet nunc vel, varius elit.Vivamus aliquet ex ut ex efficitur
+                    finibus ut vel ipsum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla vel
+                    laoreet arcu. Cras vitae dui nisl. Vestibulum quam dolor, malesuada id consectetur ac, fringilla
+                    lobortis odio. Morbi nec leo varius, iaculis dui eu, porttitor eros. Fusce blandit pellentesque consequat.
+                </p>
             </div>
             <div>
                 <h3>Projetos</h3>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vehicula tristique luctus. 
+                    Nunc porttitor justo mollis, imperdiet nunc vel, varius elit.Vivamus aliquet ex ut ex efficitur
+                    finibus ut vel ipsum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla vel
+                    laoreet arcu. Cras vitae dui nisl. Vestibulum quam dolor, malesuada id consectetur ac, fringilla
+                    lobortis odio. Morbi nec leo varius, iaculis dui eu, porttitor eros. Fusce blandit pellentesque consequat.
+                </p>
             </div>
         </div>
     )
