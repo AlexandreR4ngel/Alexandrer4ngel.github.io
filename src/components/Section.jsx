60213393709c1186a80ced9449d1022e4a1aea02
Alexandre Magno Rangel <99764226+AlexandreR4ngel@users.noreply.github.com>
2023-06-28 17:31:06 +0000

    Adicionando animação na Main

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -3,9 +3,9 @@ import './Section.style.css';
 export default function Section() {
     return (
         <div className="section">
-            <h2>Bem-vindo.</h2>
-            <h3>Alexandre Magno Rangel - Desenvolvedor web fullstack</h3>
-            <h4>Eu construo produtos acessíveis, inclusivos e experiências digitais para a web.</h4>
+            <h2 className='lineUp'>Bem-vindo.</h2>
+            <h3 className='lineUp2'>Alexandre Magno Rangel - Desenvolvedor web fullstack</h3>
+            <h4 className='lineUp3'>Eu construo produtos acessíveis, inclusivos e experiências digitais para a web.</h4>
         </div>
     )
 }
